1cf36ed64967a6de5ab52e9c356b0351e7e115e1
yukiukisato <yukiukisato@yahoo.co.jp>
2021-04-15 16:41:02 +0900

    ユーザー登録機能

--- db/migrate/20210414125615_create_posts.rb
+++ db/migrate/20210414125615_create_posts.rb
@@ -1,8 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
+      t.string :name,           null: false
+
 
       t.timestamps
+
     end
   end
 end
