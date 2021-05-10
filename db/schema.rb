51485574a806c9a80acb9ab01675d00e476aa6ee
yukiukisato <yukiukisato@yahoo.co.jp>
2021-05-10 15:05:49 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,6 @@ ActiveRecord::Schema.define(version: 2021_05_04_023817) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
